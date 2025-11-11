bfb6d7cce76a868594f9e948945d2d74a4bf75ab
K201\r7g2a1 <gangbenhucilang@gmail.com>
2025-11-11 11:50:10 +0900

    テスト

--- Assets/MyGame/Sctipts/CameraControl.cs
+++ Assets/MyGame/Sctipts/CameraControl.cs
@@ -7,7 +7,7 @@ public class CameraControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ///AAA
     }
 
     // Update is called once per frame
