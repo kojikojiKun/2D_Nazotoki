52496147c39eae45c8dfce5c7ae90f89fc597ead
K201\r7g2a1 <gangbenhucilang@gmail.com>
2025-11-11 12:30:25 +0900

    カメラ追従スクリプト作成

--- Assets/MyGame/Sctipts/CameraControl.cs
+++ Assets/MyGame/Sctipts/CameraControl.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] PlayerController m_target; //追跡対象のオブジェクト.
+    [SerializeField] private Vector3 offset; //追跡対象からの距離.
+    private bool m_canChasing; //追跡フラグ.
+
+    //追跡中止.
+    public void StopChase()
     {
-        ///AAA
+        m_canChasing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //目標から一定距離離して追跡.
+        if (m_canChasing == true)
+        {
+            transform.position = m_target.transform.position + new Vector3(offset.x, offset.y, offset.z);
+        }
     }
 }
